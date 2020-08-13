5272b3d6292648b7ece1a25c0a78c98aceceb856
RobertAKARobin <contact@robertakarobin.com>
2020-08-13 08:54:06 -0500

    Initial boilerplate

---
+++ index.vue
@@ -0,0 +1,15 @@
+<template>
+  <lib-read-more>
+    Hello world
+  </lib-read-more>
+</template>
+
+<script>
+  import libReadMore from './lib-readmore';
+
+  export default {
+    components: {
+      libReadMore,
+    }
+  }
+</script>
